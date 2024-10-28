c9284fbcbc9371bf9ea459157eacdf6bbbc0e9bf
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-10-28 15:38:45 +0500

    Task5

--- Tyuiu.AntonovNS.Sprint3.Task5.V16.Lib/DataService.cs
+++ Tyuiu.AntonovNS.Sprint3.Task5.V16.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.AntonovNS.Sprint3.Task5.V16.Lib
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
-            double p = 1;
+            double p = 0;
             for (int k = startValue1; k <= stopValue1; k++)
             {
                 for (int i = startValue2; i <= stopValue2; i++)
